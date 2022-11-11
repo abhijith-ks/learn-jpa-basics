b46c615b17373b71d5153ce7f25832c358459808
abhijith-ks <nandu.abhi2@gmail.com>
2022-11-11 10:20:12 +0530

    Inital Commit

---
+++ src/main/java/io/abhijith/learnjpabasics/LearnJpaBasicsApplication.java
@@ -0,0 +1,13 @@
+package io.abhijith.learnjpabasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/abhijith/learnjpabasics/LearnJpaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.abhijith.learnjpabasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
